f427aaad67d5b9088cd730b02b78405f4a618bf7
Miles Armé <52762433+HereticalSilence@users.noreply.github.com>
2023-01-26 11:55:05 +0000

    commit 3

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-basePrice = 1.5
-userAge = ""
-while userAge.lower() != "quit":
-    userAge = input("Enter an age: ")
-    if int(userAge) >= 16:
-        basePrice = 2.50
-    print ("The ticket price is £"+str(basePrice)+"0")
+s=3
+print (6*(s**2))
--- tkinter test.py
+++ tkinter test.py
@@ -1,13 +1,8 @@
 import tkinter as tkinter
 window = tkinter.Tk()
-for x in range(4):
-    for y in range(2):
-        framegrid = tkinter.Frame(
-        master=window,
-        relief=tkinter.SUNKEN,
-        borderwidth=1.5
-        )
+for x in range(5):
+    for y in range(7):
+        framegrid = tkinter.Frame(master=window, relief=tkinter.SUNKEN, borderwidth=1.5)
         framegrid.grid(row=x, column=y, padx=5, pady=5)
-        labelgrid = tkinter.Label(master=framegrid, text=f"Row no. {x}\nColumn no. {y}")
-        labelgrid.pack(padx=3, pady=3)
-        window.mainloop()
+        labelgrid = tkinter.Label(master=framegrid, text=f"Row no. {x}\nColumn no. {y}").pack(padx=3, pady=3)
+window.mainloop()
